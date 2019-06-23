80c61dd590c76a89149f29e0bd5c942bbdb3e1fc
Eugenie0001 <five.airport.extreme@gmail.com>
2019-06-24 00:17:43 +0500

    commit

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
 $from = 'Demo contact form <e.roess@experimentmysite.ml>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <e.roess@experimentmysite.ml>';
+$sendTo = 'Demo contact form <five.airport.extreme@gmail.com>';
 
 // subject of the email
 $subject = 'New message from contact form';
